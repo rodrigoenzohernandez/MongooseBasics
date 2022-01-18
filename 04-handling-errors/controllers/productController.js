01469b7f55960063fbe18e4a0d49edae25cdcf4e
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-18 10:29:13 -0300

    delete all particulary try catch

--- 04-handling-errors/controllers/productController.js
+++ 04-handling-errors/controllers/productController.js
@@ -4,33 +4,25 @@ const AppError = require("../utilities/AppError");
 
 const productController = {
   async getProducts(req, res) {
-    try {
-      const regexName = new RegExp(req.query.name, "i");
-      const regexCat = new RegExp(req.query.category, "i");
-      const filter = {
-        name: { $regex: regexName },
-        category: { $regex: regexCat },
-      };
+    const regexName = new RegExp(req.query.name, "i");
+    const regexCat = new RegExp(req.query.category, "i");
+    const filter = {
+      name: { $regex: regexName },
+      category: { $regex: regexCat },
+    };
 
-      const products = await Product.find(filter);
+    const products = await Product.find(filter);
 
-      res.send(products);
-    } catch (error) {
-      next(error);
-    }
+    res.send(products);
   },
   async getProductDetail(req, res, next) {
-    try {
-      const product = await Product.findById(req.params.id);
-      if (product) res.send(product);
-      else {
-        throw new AppError("Product Not Found", 404);
-      }
-    } catch (error) {
-      next(error);
+    const product = await Product.findById(req.params.id);
+    if (product) res.send(product);
+    else {
+      throw new AppError("Product Not Found", 404);
     }
   },
-  async createProduct(req, res) {
+  async createProduct(req, res, next) {
     const p = new Product(req.body);
 
     p.save()
@@ -50,34 +42,25 @@ const productController = {
       new: true,
       runValidators: true,
     };
+    const updated = await Product.findByIdAndUpdate(id, newDocument, options);
 
-    try {
-      const updated = await Product.findByIdAndUpdate(id, newDocument, options);
-
-      if (updated) res.send(updated);
-      else {
-        throw new AppError(
-          "No product was found with that ID, so nothing was updated",
-          404
-        );
-      }
-    } catch (error) {
-      next(error);
+    if (updated) res.send(updated);
+    else {
+      throw new AppError(
+        "No product was found with that ID, so nothing was updated",
+        404
+      );
     }
   },
   async deleteProduct(req, res, next) {
-    try {
-      const deleted = await Product.findByIdAndRemove(req.body._id);
+    const deleted = await Product.findByIdAndRemove(req.body._id);
 
-      if (deleted) res.send(deleted);
-      else {
-        throw new AppError(
-          "No product was found with that ID, so nothing was deleted",
-          404
-        );
-      }
-    } catch (error) {
-      next(error);
+    if (deleted) res.send(deleted);
+    else {
+      throw new AppError(
+        "No product was found with that ID, so nothing was deleted",
+        404
+      );
     }
   },
 };
