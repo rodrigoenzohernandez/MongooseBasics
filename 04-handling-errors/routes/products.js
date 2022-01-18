3dc5cb7f0dfe12cbe308a4eb78e9e176971e7aa3
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-18 10:29:25 -0300

    implement new wrapAsync function

--- 04-handling-errors/routes/products.js
+++ 04-handling-errors/routes/products.js
@@ -3,14 +3,25 @@ var router = express.Router();
 
 const productController = require('../controllers/productController')
 
-router.get('/', productController.getProducts);
+router.get('/', wrapAsync(productController.getProducts));
 
-router.get('/:id', productController.getProductDetail);
+router.get('/:id', wrapAsync(productController.getProductDetail));
 
-router.post('/', productController.createProduct);
+router.post('/', wrapAsync(productController.createProduct));
 
-router.put('/', productController.updateProduct);
+router.put('/', wrapAsync(productController.updateProduct));
 
-router.delete('/', productController.deleteProduct);
+router.delete('/', wrapAsync(productController.deleteProduct));
+
+/**
+ * This function receives an async function and executes it.
+ * @param {function} fn Async function 
+ * @returns 
+ */
+ function wrapAsync(fn) {
+    return function (req, res, next) {
+      fn(req, res, next).catch((e) => next(e));
+    };
+  }
 
 module.exports = router;
