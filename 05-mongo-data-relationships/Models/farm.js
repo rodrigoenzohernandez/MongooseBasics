7a929b2d4e72cc4d638593864b2e6f4560dcfe28
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-19 16:10:38 -0300

    add one to many example

---
+++ 05-mongo-data-relationships/Models/farm.js
@@ -0,0 +1,45 @@
+// //mongo connection
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/relationships");
+  console.log("Connection open!");
+}
+
+const farmSchema = new Schema({
+  name: String,
+  city: String,
+  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+});
+
+const productSchema = new Schema({
+  name: String,
+  price: Number,
+  season: {
+    type: String,
+    enum: ["Spring", "Summer", "Fall", "Winter"],
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+const Farm = mongoose.model("Farm", farmSchema);
+
+const makeFarm = async () => {
+  const farm = new Farm({
+    name: "Don Mario",
+    city: "Burzaco",
+  });
+  const Sandia = await Product.findOne({ name: "Sandia" });
+
+  farm.products.push(Sandia);
+
+  await farm.save();
+
+  console.log(farm);
+};
+
+makeFarm();
