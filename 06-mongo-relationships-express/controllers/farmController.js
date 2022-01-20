c01e32d77c03216e961bcbfc46b36c627d72733d
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-20 11:21:25 -0300

    add method to add product to a farm

--- 06-mongo-relationships-express/controllers/farmController.js
+++ 06-mongo-relationships-express/controllers/farmController.js
@@ -1,4 +1,5 @@
 const Farm = require('../models/farm')
+const Product = require('../models/product')
 
 const farmController = {
   async getFarms(req, res) {
@@ -13,7 +14,19 @@ const farmController = {
       const farm = new Farm(req.body);
       await farm.save()
       res.send(farm)
-  }
+  },
+  async addProductToFarm(req, res){
+      const {id} = req.params;
+      const productId = req.query.productId
+      const farm = await Farm.findById(id);
+      const product = await Product.findById(productId);
+
+      farm.products.push(product);
+      product.farm = farm;
+      await farm.save();
+      await product.save();
+      res.send(farm)
+}
 };
 
 module.exports = farmController;
