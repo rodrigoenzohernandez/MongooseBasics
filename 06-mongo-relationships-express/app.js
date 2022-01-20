47806d2e9a53f0159462f3048f2f35233d6b49e6
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-20 10:28:56 -0300

    change db name

--- 06-mongo-relationships-express/app.js
+++ 06-mongo-relationships-express/app.js
@@ -27,7 +27,7 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/farmStand");
+  await mongoose.connect("mongodb://localhost:27017/farmUpdatedDB");
   console.log("Connection open!");
 }
 
