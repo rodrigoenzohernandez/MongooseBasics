080f485e1d446659b743c1a51ebfbd38faddef4b
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-24 16:36:21 -0300

    add session project practice

---
+++ 08-session-and-flash/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+let counter;
+
+const sessionOptions = {
+  secret: "my-secret",
+  resave: false,
+  saveUninitialized: false,
+};
+
+app.use(session(sessionOptions));
+
+app.get("/viewcount", (req, res) => {
+  req.session.count ? (req.session.count += 1) : (req.session.count = 1);
+
+  res.send(`You have visited this page ${req.session.count} times`);
+});
+
+app.get("/register", (req, res) => {
+  const { username = "unknown" } = req.query;
+  console.log(username);
+  req.session.username = username;
+  res.redirect('/greet')
+});
+
+app.get("/greet", (req, res) => {
+    console.log(req.session.username);
+  const { username } = req.session;
+  res.send(`Hi, I'm ${username}`);
+});
+
+app.listen(3000, () => {
+  console.log("SERVING");
+});
