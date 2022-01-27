e50afc6f8b1faa883a585df38c23f05434011b9e
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-27 09:22:46 -0300

    create user model

--- 09-authentication/03-PassportDemo/models/user.js
+++ 09-authentication/03-PassportDemo/models/user.js
@@ -1,32 +1,42 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema
 
-const userSchema = new mongoose.Schema({
-  username: {
+
+// const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  email: {
     type: String,
-    required: [true, "Username cannot be blank"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password cannot be blank"],
-  },
+    required: [true, "Email cannot be blank"],
+    unique: true
+  }
 });
 
-userSchema.statics.findAndValidate = async function (username, password) {
-  const foundUser = await this.findOne({ username });
+userSchema.plugin(passportLocalMongoose)
 
-  let logged = false;
-  if (foundUser) {
-    logged = await bcrypt.compare(password, foundUser.password);
-  }
-  return logged ? foundUser : false;
-};
+/**
+ * Passport-Local Mongoose will add a username, hash and salt field to store the username, the hashed password and the salt value.
 
-userSchema.pre('save', async function(next){
-    //this refers to the particular instance
-    if(!this.isModified(this.password)) return next()
-    this.password = await bcrypt.hash(this.password, 12)
-    next()
-})
+Additionally Passport-Local Mongoose adds some methods to your Schema. See the API Documentation section for more details.
+ */
+
+
+// userSchema.statics.findAndValidate = async function (username, password) {
+//   const foundUser = await this.findOne({ username });
+
+//   let logged = false;
+//   if (foundUser) {
+//     logged = await bcrypt.compare(password, foundUser.password);
+//   }
+//   return logged ? foundUser : false;
+// };
+
+// userSchema.pre('save', async function(next){
+//     //this refers to the particular instance
+//     if(!this.isModified(this.password)) return next()
+//     this.password = await bcrypt.hash(this.password, 12)
+//     next()
+// })
 
 module.exports = mongoose.model("User", userSchema);
